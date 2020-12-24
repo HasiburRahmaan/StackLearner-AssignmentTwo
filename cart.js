2a9cd09384323b2e2eab61986fbde6e45b849e89
Hasibur Rahman <hasibur.rahmaan@gmail.com>
2020-12-24 19:48:42 +0600

    initialize cart.js

---
+++ cart.js
@@ -0,0 +1,24 @@
+let cartData = {
+  cartItem: [],
+  totalPrice: 0,
+};
+let itemsArray = [
+  {
+    id: 1,
+    name: "Product One",
+    price: 10,
+  },
+  {
+    id: 2,
+    name: "Product Two",
+    price: 15,
+  },
+  {
+    id: 3,
+    name: "Product Three",
+    price: 20,
+  },
+];
+
+function addItemIntoCartHandler(productId) {}
+function removeItemIntoCartHandler(productId) {}
